7e94782b706aef31125b7b2169b260d01158e3e1
hyunseo0803 <abcdefghjk5@naver.com>
2024-01-06 00:53:50 +0900

    Initial commit

---
+++ src/main/java/a/b/c/RmSolutionApplication.java
@@ -0,0 +1,13 @@
+package a.b.c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/a/b/c/RmSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package a.b.c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
