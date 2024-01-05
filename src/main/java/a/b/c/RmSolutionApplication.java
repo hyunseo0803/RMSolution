0e15f205802484ad08da0d5362676c2396d59280
hyunseo0803 <abcdefghjk5@naver.com>
2024-01-06 01:03:51 +0900

    jsp 생성 및 실행 테스트

--- src/main/java/a/b/c/RmSolutionApplication.java
+++ src/main/java/a/b/c/RmSolutionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RmSolutionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RmSolutionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RmSolutionApplication.class, args);
+    }
 
 }
--- src/main/java/a/b/c/controller.java
+++ src/main/java/a/b/c/controller.java
@@ -1,2 +1,13 @@
-package a.b.c;public class controller {
+package a.b.c;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controller {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index.jsp";
+    }
 }
