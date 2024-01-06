30f2db0b3e1a21262750751c8e72e92da10f7a59
hyunseo0803 <abcdefghjk5@naver.com>
2024-01-06 21:08:54 +0900

    Mapper.xml 에 접근 하여 SQL 수행 - 회원 추가, 아이디 존재 SELECT, 비밀 번호 SELECT

---
+++ src/main/java/a/b/c/dao/MemberDAO.java
@@ -0,0 +1,19 @@
+package a.b.c.dao;
+
+import a.b.c.vo.MemberVO;
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface MemberDAO {
+
+    //회원 추가
+    void insertMember(MemberVO member);
+
+    //존재 하는 계정 인지
+    String isIdDuplicate(String mid);
+
+    //비밀 번호 일치 하는지
+    String findByIdpw(String mid);
+
+}
