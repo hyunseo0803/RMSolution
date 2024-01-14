ca7f5c43a9a8bcbd30b95e235a8d2a72cd2b6ad7
hyunseo0803 <abcdefghjk5@naver.com>
2024-01-14 17:26:09 +0900

    1. 단위 변환 이슈로 usage db 칼럼 삭제 -> 사용자에게 총 사용량 보여질때 sumUsageById 메서드 이용 2. 서비스 별 다른 용량 제공 및 용량 늘리기 기능 -> 각 사용자의 사용 가능한 용량 db 칼럼 생성 3. 용량 늘리기 기능 구현 , 늘릴 시, 추가되는 요금 계산 4. 기간 늘리기 기능 구현

--- src/main/java/a/b/c/controller/dashBoard.java
+++ src/main/java/a/b/c/controller/dashBoard.java
@@ -7,8 +7,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+import java.util.*;
+
 
 @Controller
 public class dashBoard {
@@ -16,20 +18,32 @@ public class dashBoard {
     @Autowired
     ServiceService serviceService;
 
+
     //DashBoard
     @GetMapping("/dashBoard")
     public String dashboardPage(@AuthenticationPrincipal User user, Model model) {
         model.addAttribute("mid", user.getUsername());
-        ServiceVO service= serviceService.findServiceByUserId(user.getUsername());
-        boolean isService=service!=null;
+        ServiceVO service = serviceService.findServiceByUserId(user.getUsername());
+
+        boolean isService = service != null;
         model.addAttribute("isService", isService);
 
-        if(isService){
-            model.addAttribute("sName",service.getKindof());
-            model.addAttribute("sUsercnt",service.getKindof());
-            model.addAttribute("sStartdate",service.getKindof());
-            model.addAttribute("sEnddate",service.getKindof());
-            model.addAttribute("sUsage",service.getUsage());
+        if (isService) {
+            Integer usage = serviceService.sumUsageById(user.getUsername());
+            String usageBTtoTB;
+            if(usage!=null){
+                usageBTtoTB = serviceService.convertBytes(usage);
+            }
+            else{
+                usageBTtoTB="0";
+            }
+
+            model.addAttribute("sName", service.getKindof());
+            model.addAttribute("sUsername", service.getUsername());
+            model.addAttribute("sStartdate", service.getStartdate());
+            model.addAttribute("sEnddate", service.getEnddate());
+            model.addAttribute("sUsage", usageBTtoTB);
+            model.addAttribute("sVolume", service.getVolume());
 
             boolean isScompany = service.getCompany() != null;
             boolean isSphone = service.getPhone() != null;
@@ -38,29 +52,84 @@ public class dashBoard {
             model.addAttribute("isSphone", isSphone);
             model.addAttribute("isSemail", isSemail);
 
-            if(isScompany){
-                model.addAttribute("sCompany",service.getCompany());
+            if (isScompany) {
+                model.addAttribute("sCompany", service.getCompany());
             }
-            if(isSphone){
-                model.addAttribute("sPhone",service.getPhone());
+            if (isSphone) {
+                model.addAttribute("sPhone", service.getPhone());
             }
-            if(isSemail){
-                model.addAttribute("sEmail",service.getEmail());
+            if (isSemail) {
+                model.addAttribute("sEmail", service.getEmail());
             }
         }
         return "dashBoard";
     }
 
+
     @GetMapping("/service_apply")
-    public String serviceApplyView(){
+    public String serviceApplyView() {
         return "serviceApply";
     }
 
+    @GetMapping("/volume_extend")
+    public String extendVolumeView() {
+        return "extendVolume";
+    }
+
+    @PostMapping("/extendVolume")
+    public String extendVolue(@AuthenticationPrincipal User user, int addvolume) {
+        int volume = serviceService.findServiceByUserId(user.getUsername()).getVolume();
+        int newvolume = volume + addvolume;
+
+        serviceService.saveNewVolume(user.getUsername(), newvolume);
+        return "success";
+    }
+
+    @ResponseBody
+    @GetMapping("/confirmPay/{addvolume}")
+    public Map<String, Object> confirmPay(@AuthenticationPrincipal User user, @PathVariable int addvolume) {
+        String servicename = serviceService.findServiceByUserId(user.getUsername()).getKindof();
+        int totalpay = 0;
+        if (servicename.equals("basic")) {
+            totalpay = addvolume * 4000;
+        } else if (servicename.equals("standard")) {
+            totalpay = addvolume * 6000;
+        } else if (servicename.equals("premium")) {
+            totalpay = addvolume * 8000;
+        }
+        Map<String, Object> confirmpay = new HashMap<>();
+        confirmpay.put("totalpay", totalpay);
+        confirmpay.put("servicename", servicename);
+        return confirmpay;
+    }
+
     @PostMapping("/service_apply")
-    public String serviceApply(ServiceVO service,@AuthenticationPrincipal User user){
-        serviceService.insertService(service,user.getUsername());
+    public String serviceApply(ServiceVO service, @AuthenticationPrincipal User user) {
+        serviceService.insertService(service, user.getUsername());
         return "redirect:/dashBoard";
     }
 
+    @PostMapping("/extendPeriod")
+    @ResponseBody
+    public Map<String, Object> extendPeriod(@AuthenticationPrincipal User user, @RequestParam int howMonth) {
+        ServiceVO service = serviceService.findServiceByUserId(user.getUsername());
+        LocalDate enddate = service.getEnddate();
+
+        LocalDate newEnddate = enddate.plusMonths(howMonth);
+        boolean checking;
+
+        try {
+            serviceService.saveNewEnddate(newEnddate, user.getUsername());
+            checking = true;
+        } catch (Exception e) {
+            checking = false;
+        }
+
+        Map<String, Object> check = new HashMap<>();
+        check.put("check", checking);
+        return check;
+
+
+    }
 
 }
