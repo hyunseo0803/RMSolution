db22ca839f09b06c2568f5b14322e765094432df
hyunseo0803 <abcdefghjk5@naver.com>
2024-01-14 17:59:50 +0900

    1. 구독 기간 연장- enddate 새로 저장 update 2. 용량 늘리기- volume 새로 저장 update 3. 사용자가 업로드한 파일 사이즈의 총 합 select 4. 신청한 서비스 정보 insert 5. byte 자동 단위 변환

--- src/main/java/a/b/c/service/ServiceService.java
+++ src/main/java/a/b/c/service/ServiceService.java
@@ -5,13 +5,15 @@ import a.b.c.vo.MemberVO;
 import a.b.c.vo.ServiceVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.text.DecimalFormat;
+import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
 public class ServiceService {
     @Autowired
     ServiceDAO serviceDAO;
+
 
     @Autowired
     MemberService memberService;
@@ -20,18 +22,56 @@ public class ServiceService {
         return serviceDAO.findServiceByUserId(userId);
     }
 
+    public Integer sumUsageById(String userId) {
+        return serviceDAO.sumUsageById(userId);
+    }
+
+    public void saveNewEnddate(LocalDate newenddate, String mid) {
+        serviceDAO.saveNewEnddate(newenddate, mid);
+    }
+
+    public void saveNewVolume(String userId, int newvolume) {
+        serviceDAO.saveNewVolume(userId, newvolume);
+    }
+
+    public String convertBytes(long bytes) {
+        String[] units = {"B", "KB", "MB", "GB", "TB"};
+        int unitIndex = 0;
+        double size = bytes;
+
+        while (size >= 1024 && unitIndex < units.length - 1) {
+            size /= 1024;
+            unitIndex++;
+        }
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        return df.format(size) + " " + units[unitIndex];
+    }
+
     public void insertService(ServiceVO service, String mid) {
+        int volume;
+        if (service.getKindof().equals("basic")) {
+            volume = 3;
+        } else if (service.getKindof().equals("standard")) {
+            volume = 6;
+        } else {
+            volume = 10;
+        }
+
+        int month = service.getMonth();
+        LocalDate sEnddate = service.getStartdate().plusMonths(month);
+
         Optional<MemberVO> memberOptional = memberService.findOne(mid);
         memberOptional.ifPresent(member -> {
             service.setMid(member.getMid());
             service.setKindof(service.getKindof());
-            service.setUsercnt(service.getUsercnt());
-            service.setDate();
-            service.setMonth(service.getMonth());
+            service.setUsername(service.getUsername());
+            service.setStartdate();
+            service.setEnddate(sEnddate);
             service.setCompany(service.getCompany());
             service.setPhone(service.getPhone());
             service.setEmail(service.getEmail());
-
+            service.setVolume(volume);
             serviceDAO.insertService(service);
         });
 
