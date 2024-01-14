534ccb81c722a279505b1fb45fc8426e2a9e9eb1
hyunseo0803 <abcdefghjk5@naver.com>
2024-01-14 17:55:45 +0900

    스프링 부트 시작시, 사용자 파일 저장할 files 폴더 자동 생성

--- src/main/java/a/b/c/RmSolutionApplication.java
+++ src/main/java/a/b/c/RmSolutionApplication.java
@@ -1,7 +1,10 @@
 package a.b.c;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class RmSolutionApplication {
@@ -10,4 +13,27 @@ public class RmSolutionApplication {
         SpringApplication.run(RmSolutionApplication.class, args);
     }
 
+    @PostConstruct
+    private void postConstruct() {
+        String userdir = System.getProperty("user.dir");
+        String mydir = "\\src\\main\\webapp\\WEB-INF\\views\\files";
+        String folderPath = userdir + mydir;
+
+        createFolder(folderPath);
+    }
+
+    private void createFolder(String folderPath) {
+        File folder = new File(folderPath);
+
+        if (!folder.exists()) {
+            boolean result = folder.mkdirs();
+            if (result) {
+                System.out.println("폴더가 생성되었습니다: " + folder.getAbsolutePath());
+            } else {
+                System.err.println("폴더 생성에 실패했습니다.");
+            }
+        } else {
+            System.out.println("폴더가 이미 존재합니다: " + folder.getAbsolutePath());
+        }
+    }
 }
