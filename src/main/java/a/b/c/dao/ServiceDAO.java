f26468481af92c6adf50b575fd4ad750985993ad
hyunseo0803 <abcdefghjk5@naver.com>
2024-01-14 17:58:07 +0900

    1. 구독 기간 연장- enddate 새로 저장 update 2. 용량 늘리기- volume 새로 저장 update 3. 사용자가 업로드한 파일 사이즈의 총 합 select

--- src/main/java/a/b/c/dao/ServiceDAO.java
+++ src/main/java/a/b/c/dao/ServiceDAO.java
@@ -2,12 +2,20 @@ package a.b.c.dao;
 
 import a.b.c.vo.ServiceVO;
 import org.apache.ibatis.annotations.Mapper;
+import java.time.LocalDate;
 
 @Mapper
 public interface ServiceDAO {
 
     void insertService(ServiceVO service);
 
+    void saveNewEnddate(LocalDate newenddate, String mid);
+
+    void saveNewVolume(String mid, int newvolume);
+
+    Integer sumUsageById(String mid);
+
     ServiceVO findServiceByUserId(String mid);
+
 
 }
